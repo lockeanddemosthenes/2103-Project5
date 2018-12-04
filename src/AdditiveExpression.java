1de69b6e2493864ac66c37cb141739b08b899c27
TrumanL <truman.larson@gmail.com>
2018-12-04 18:43:27 -0500

    Finished class structure

--- src/AdditiveExpression.java
+++ src/AdditiveExpression.java
@@ -1,4 +1,4 @@
 
 public class AdditiveExpression extends SimpleCompoundExpression {
-
+	
 }
--- src/SimpleCompoundExpression.java
+++ src/SimpleCompoundExpression.java
@@ -1,4 +1,41 @@
+import java.util.List;
 
-public abstract class SimpleCompoundExpression extends AbstractCompoundExpression {
+public class SimpleCompoundExpression implements CompoundExpression {
+	private CompoundExpression _parent;
+	private List<Expression> _children;
+ 	
+	@Override
+	public CompoundExpression getParent() {
+		return _parent;
+	}
 
+	@Override
+	public void setParent(CompoundExpression parent) {
+		_parent = parent;
+	}
+
+	@Override
+	public Expression deepCopy() {
+		SimpleCompoundExpression  copy = new SimpleCompoundExpression();
+		 for (Expression child : _children) {
+			 copy._children.add((Expression) child.deepCopy()); // recurse
+		 }
+		 return copy;
+	}
+
+	@Override
+	public void flatten() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void convertToString(StringBuilder stringBuilder, int indentLevel) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void addSubexpression(Expression subexpression) {
+		_children.add(subexpression);
+	}
 }
